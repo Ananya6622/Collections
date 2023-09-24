7192f8af3558b7402ca3d354122e9ccd8e67718b
Ananya6622 <ananyaetti6622@gmail.com>
2023-09-24 11:11:25 +0530

    added

---
+++ ArrayList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Collections
+{
+    public static void Main()
+    {
+        // Create an ArrayList
+        ArrayList arrayList = new ArrayList();
+
+        // Create - Add items to the ArrayList
+        arrayList.Add("Apple");
+        arrayList.Add("Banana");
+        arrayList.Add("Cherry");
+
+        // Read - Access items from the ArrayList
+        Console.WriteLine("ArrayList Items:");
+        foreach (var item in arrayList)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Update - Modify an item in the ArrayList
+        arrayList[1] = "Blueberry";
+
+        // Read - Access the updated ArrayList
+        Console.WriteLine("\nUpdated ArrayList:");
+        foreach (var item in arrayList)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Delete - Remove an item from the ArrayList
+        arrayList.Remove("Cherry");
+
+        // Read - Access the final ArrayList
+        Console.WriteLine("\nArrayList after Deletion:");
+        foreach (var item in arrayList)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ Dictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static void Main()
+    {
+        // Create a Dictionary with string keys and int values
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+        // Create - Add key-value pairs to the Dictionary
+        dictionary.Add("Ananya", 25);
+        dictionary.Add("Honey", 30);
+        dictionary.Add("Suhas", 35);
+
+        // Read - Access values by keys
+        Console.WriteLine("Dictionary Items:");
+        foreach (var key in dictionary.Keys)
+        {
+            Console.WriteLine($"{key}: {dictionary[key]} years old");
+        }
+
+        // Update - Modify a value in the Dictionary
+        dictionary["Suhas"] = 31;
+
+        // Read - Access the updated Dictionary
+        Console.WriteLine("\nUpdated Dictionary:");
+        foreach (var key in dictionary.Keys)
+        {
+            Console.WriteLine($"{key}: {dictionary[key]} years old");
+        }
+
+        // Delete - Remove a key-value pair from the Dictionary
+        dictionary.Remove("Honey");
+
+        // Read - Access the final Dictionary
+        Console.WriteLine("\nDictionary after Deletion:");
+        foreach (var key in dictionary.Keys)
+        {
+            Console.WriteLine($"{key}: {dictionary[key]} years old");
+        }
+    }
+}
